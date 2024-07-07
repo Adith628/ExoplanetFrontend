edc8b04914ee2a36de3050b481a3a1d6cfef261c
Adithyan T <adithsreedhar628@gmail.com>
2024-07-07 17:30:19 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [file, setFile] = useState(null);
@@ -33,22 +35,36 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Exoplanet Habitability Prediction</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} accept=".csv" required />
-        <button type="submit">Upload and Predict</button>
-      </form>
-      <div className="results">
-        <h2>Prediction Results:</h2>
-        <ul>
-          {results.map((result, index) => (
-            <li key={index}>
-              <strong>{result.kepoi_name}</strong>: {result.habitable}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="App ">
+      <BackgroundGradientAnimation>
+        <NavBar />
+        <div className="flex absolute z-10 h-full flex-col inset-0 items-center justify-center ">
+          <div className="">
+            <h1 className="flex p-10 text-7xl text-center text-white font-bold tracking-tighter">
+              Exoplanet Habitability <br /> Prediction
+            </h1>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="file"
+                onChange={handleFileChange}
+                accept=".csv"
+                required
+              />
+              <button type="submit">Upload and Predict</button>
+            </form>
+            <div className="results">
+              <h2>Prediction Results:</h2>
+              <ul>
+                {results.map((result, index) => (
+                  <li key={index}>
+                    <strong>{result.kepoi_name}</strong>: {result.habitable}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </BackgroundGradientAnimation>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="w-full sticky text-xl font-bold p-6 z-10 bg-gray-600 bg-opacity-10">
+      AstroNeT
+    </div>
+  );
+};
+
+export default NavBar;
